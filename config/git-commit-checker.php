2d74dceefc6cb8c6089a8963d710999d91b9b201
HaiNemo <hainq@flydinotech.com>
2022-08-23 00:10:29 +0700

    Update code base

---
+++ config/git-commit-checker.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Console\Commands\PreCommitHook;
+
+return [
+    'enabled' => env('GIT_COMMIT_CHECKER_ENABLED', true),
+    'psr2' => [
+        'standard' => __DIR__ . '/../phpcs.xml',
+        'ignored' => [],
+    ],
+    'hooks' => [
+        'pre-commit' => PreCommitHook::class,
+    ],
+];
